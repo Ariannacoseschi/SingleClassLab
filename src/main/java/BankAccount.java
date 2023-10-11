34b0b383db7fe151f38af371c66a68ce118f19b7
FIRST_arianna LAST_coseschi <ariannacoseschi@gmail.com>
2023-10-11 17:29:29 +0100

    mvp in progress

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,2 +1,37 @@
+import java.time.LocalDate;
+
 public class BankAccount {
+    public String firstName;
+    public String lastName;
+    public LocalDate dateOfBirth;
+    public String accountNumber;
+    public double balance;
+
+    public BankAccount(String firstName, String lastName, String dateOfBirth, String accountNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.accountNumber = accountNumber;
+        this.balance = 0.0;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    public String getDate0fBirth() {
+        return dateOfBirth;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+
 }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+
 public class BankAccountTest {
+
+    @Test
+    public void testInitialBalance(){
+
+
+
+    }
+
+
+
+
+
+
+
+
 }
